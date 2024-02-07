49281d821723163ea246d02b7321ec61afb1e4bc
NishitVAts <nishitvats@icloud.com>
2024-02-08 01:09:38 +0530

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Nishit Vats on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
